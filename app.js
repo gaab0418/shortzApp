2d7d3333cabe5ad055611345e82f4343ac9a3dba
gaab1804 <zgabrielw.sz13@gmail.com>
2026-02-19 22:20:41 -0300

    Aula 2

--- app.js
+++ app.js
@@ -23,19 +23,26 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
+
+const sequelize = require('./config/database');
+
+sequelize
+	.authenticate()
+	.then(() => console.log('Banco OK'))
+	.catch((err) => console.error('Erro no banco: ', err));
 
 module.exports = app;
